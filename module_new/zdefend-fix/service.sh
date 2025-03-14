9c731ca5c6be300a0e58df5cd73de38a294cc881
Cuynu <cuynu@proton.me>
2025-03-14 23:17:01 +0700

    deleting /data/magisk for startup script and actions

--- module_new/zdefend-fix/service.sh
+++ module_new/zdefend-fix/service.sh
@@ -4,4 +4,6 @@ while [ "$(getprop sys.boot_completed)" != "1" ]; do
 	sleep 1
 done
 
+# Delete /data/magisk if it exists so MB doesnt failling when eKYC with error code EKYC3002-MS6998 for Magisk users
+[[ -d /data/magisk ]] && rm -r /data/magisk
 for library in $(find /data/app -name libZDefend.so) ; do sed -i 's|.zimperium|.cuynuttmb|g' $library ; done
