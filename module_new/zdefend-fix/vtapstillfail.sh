d1ca2b7d218a6ec4550f61f67b5945c03683891f
Cuynu <cuynu@proton.me>
2025-10-29 02:50:30 +0700

    feat: vtapstillfail.sh: notice for low-end devices that is failing to properly provision vtap
    Signed-off-by: Cuynu <cuynu@proton.me>

--- module_new/zdefend-fix/vtapstillfail.sh
+++ module_new/zdefend-fix/vtapstillfail.sh
@@ -1,3 +1,5 @@
 #!/system/bin/sh
-echo "VTAP provision failed! Cannot continue..."
+echo "VTAP provision failed! Cannot continue!"
+echo "If you have low-end device (eg : Vsmart Joy 2+)"
+echo "Try to reinstall module again 3 more times."
 exit 100 
