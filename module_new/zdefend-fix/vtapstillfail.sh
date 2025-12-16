b856debf093cecb771883a9645e42adff3e904d5
Cuynu <cuynu@proton.me>
2025-12-16 21:03:55 +0700

    feat: vtapstillfail.sh: open vtapfix after vtap failling to init
    Signed-off-by: Cuynu <cuynu@proton.me>

--- module_new/zdefend-fix/vtapstillfail.sh
+++ module_new/zdefend-fix/vtapstillfail.sh
@@ -1,5 +1,16 @@
 #!/system/bin/sh
+
+SYSLANGVI="$(getprop persist.sys.locale | grep vi-VN)"
+
 echo "VTAP provision failed! Cannot continue!"
 echo "If you have low-end device (eg : Vsmart Joy 2+)"
 echo "Try to reinstall module again 3 more times."
-exit 100 
+
+if [ $SYSLANGVI ]; then
+	am start -a android.intent.action.VIEW -d https://git.disroot.org/mbcp/info_vi/wiki/vtapfix
+else
+	am start -a android.intent.action.VIEW -d https://git.disroot.org/mbcp/info_en/wiki/vtapfix	
+fi
+
+exit 100
+
