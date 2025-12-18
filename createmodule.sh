ecb8075007adef73f3bb5d667e96e576660cac99
Cuynu <cuynu@proton.me>
2025-12-18 15:59:52 +0700

    feat: bump v3.8-mb-bindhosts
    Signed-off-by: Cuynu <cuynu@proton.me>

--- createmodule.sh
+++ createmodule.sh
@@ -5,16 +5,16 @@ COMMIT="$(git rev-parse --short HEAD)"
 
 echo "Creating module (*.zip)..."
 cd module_new/zdefend-fix
-zip -r mbzdefend_fix_$COMMIT.zip . >/dev/null 2>&1
+zip -r mbzdefend_fix_bindhosts_$COMMIT.zip . >/dev/null 2>&1
 cd ..
 cd ..
 mv module_new/zdefend-fix/mbzdefend_fix_*.zip mbzdefend_fix_$COMMIT.zip
 
 if [ -f mbzdefend_fix_*.zip ]; then
-	echo "Module saved to [mbzdefend_fix_$COMMIT.zip], try to flash it :>"
+	echo "Module saved to [mbzdefend_fix_bindhosts_$COMMIT.zip], try to flash it :>"
 	rm -rf module_new/zdefend-fix/*.zip
 else
-	echo "Where is [mbzdefend_fix_$COMMIT.zip] ?"
+	echo "Where is [mbzdefend_fix_bindhosts_$COMMIT.zip] ?"
 	echo "Did module actually created yet... Try fetch repo and use command again!"
 	echo "If your system doesn't have [zip], pls install it"
 fi
